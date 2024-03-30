8ab3f0ebdb9f51da2387fb45654663d0423c329b
hnewey7 <hnewey7@gmail.com>
2024-03-30 23:41:07 +0000

    Removing encrypted parameter.

--- src/ig_package/main.py
+++ src/ig_package/main.py
@@ -61,7 +61,7 @@ class IG():
 
       **NOTE: API key, username and password should be entered when initialising the IG object."""
 
-  def __init__(self,API_key:str,username:str,password:str,encrypted_enable:bool=False,watchlist_enable:bool=False) -> None:
+  def __init__(self,API_key:str,username:str,password:str,watchlist_enable:bool=False) -> None:
     # Defining header.
     self.header = {
       "Content-Type":"application/json; charset=UTF-8",
